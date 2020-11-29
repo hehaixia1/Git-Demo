a59c5c9e85490017807f1232dc800af74895a03d
${hehaixia1} <${1101923234@qq.com}>
2020-11-29 21:10:42 +0800

    第四次更改内容并且提交

--- Text_Git/src/com/domain/User.java
+++ Text_Git/src/com/domain/User.java
@@ -4,6 +4,5 @@ public class User {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("这是第二次更改提交");
-        System.out.println("在再尝试一次");
     }
 }
